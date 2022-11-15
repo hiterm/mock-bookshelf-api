c53eeab1291d5d345543ee1f185f8d937ed0892d
hiterm <hiterm23@gmail.com>
2022-11-15 22:11:18 +0900

    refactor: Switch to apollo-server-integration-next

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,31 +1,13 @@
 import { ApolloServer } from "@apollo/server";
-import { nextHandler } from "apollo-server-nextjs";
 import { addMocksToSchema } from "@graphql-tools/mock";
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import { mocks } from "../../apollo/mocks";
 import { typeDefs } from "../../apollo/typeDefs";
 import Cors from "cors";
 import { NextApiRequest, NextApiResponse } from "next";
+import { startServerAndCreateNextHandler } from "@as-integrations/next";
 
 const cors = Cors();
-
-// Helper method to wait for a middleware to execute before continuing
-// And to throw an error when an error happens in a middleware
-function runMiddleware(
-  req: NextApiRequest,
-  res: NextApiResponse,
-  fn: Function
-) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result: any) => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
-
-      return resolve(result);
-    });
-  });
-}
 
 const apolloServer = new ApolloServer({
   schema: addMocksToSchema({
@@ -34,15 +16,4 @@ const apolloServer = new ApolloServer({
   }),
 });
 
-const apolloMiddleWare = nextHandler(apolloServer);
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  // Run the middleware
-  await runMiddleware(req, res, cors);
-
-  // Rest of the API logic
-  await runMiddleware(req, res, apolloMiddleWare);
-}
+export default startServerAndCreateNextHandler(apolloServer);
